a7c7d5a14fc9a4d4841a683ecbab668990a49f03
LeonardoLobas <leonardolobas2703@gmail.com>
2025-02-20 17:38:13 -0300

    Atualizar estilos da página Home e do DayPicker; adicionar novas classes CSS para layout e responsividade

--- src/Home.tsx
+++ src/Home.tsx
@@ -6,7 +6,7 @@ import "./daypicker-overrides.css";
 const Home = () => {
     const [selected, setSelected] = useState<Date>();
     return (
-        <div className="flex-1 bg-gray-200 h-full p-8 grid grid-cols-[25%_75%] gap-4 ">
+        <div className="flex-1 bg-amber-50  h-full p-8 grid grid-cols-[25%_75%] gap-4 ">
             <div className="bg-black text-white grid place-items-center rounded-2xl ">
                 <DayPicker
                     mode="single"
@@ -14,14 +14,16 @@ const Home = () => {
                     onSelect={setSelected}
                     footer={
                         selected
-                            ? `Selected: ${selected.toLocaleDateString()}`
+                            ? `Selecionado: ${selected.toLocaleDateString()}`
                             : "Escolha um dia."
                     }
                 />
             </div>
 
-            <div className="bg-black text-white grid place-items-center rounded-2xl ">
-                <h2>Área 70%</h2>
+            <div className="bg-black text-white grid grid-cols-3 place-items-center rounded-2xl overflow-hidden">
+                <div className="col-span-1 h-full w-full bg-amber-900  "></div>
+                <div className="col-span-1 h-full w-full bg-amber-400"></div>
+                <div className="col-span-1 h-full w-full bg-amber-600"></div>
             </div>
         </div>
     );
