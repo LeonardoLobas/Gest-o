a0ce6ce8133885a9507a9de08453f95c26986bf7
LeonardoLobas <leonardolobas2703@gmail.com>
2025-02-27 17:33:54 -0300

    Refatorar animações na página Home; adicionar funcionalidade de exibição para a seção "Adicionar" e atualizar títulos para "Vendas" e "Cadastros Clientes"

--- src/Home.tsx
+++ src/Home.tsx
@@ -2,22 +2,13 @@ import { useState } from "react";
 import { DayPicker } from "react-day-picker";
 import "react-day-picker/style.css";
 import "./daypicker-overrides.css";
-import { motion, useAnimation } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 
 const Home = () => {
     const [selected, setSelected] = useState<Date>();
-    const [clicked, setClicked] = useState(false);
-    const controls = useAnimation();
 
-    const handleClick = () => {
-        if (!clicked) {
-            setClicked(true);
-            controls.start({
-                y: 170,
-                transition: { type: "spring", stiffness: 100 },
-            });
-        }
-    };
+    const [isVisible, setIsVisible] = useState(false);
+
     return (
         <div className="flex-1 bg-[#FFFDF3]h-full p-8 grid grid-cols-[25%_75%] gap-4 ">
             <div className="bg-black  text-amber-50 grid place-items-center rounded-2xl ">
@@ -34,65 +25,89 @@ const Home = () => {
             </div>
 
             <div className="bg-black text-amber-50 grid grid-cols-3 place-items-center text-lg tracking-[0.1rem] font-bold rounded-2xl overflow-hidden">
-                <motion.div
-                    className="col-span-1  border-[#373739] place-content-center h-full place-items-center w-full bg-black relative"
-                    initial={{
-                        y: -100,
-                    }}
-                    animate={{
-                        y: 0,
-                    }}
-                    transition={{
-                        duration: 0.8,
-                        ease: "easeOut",
-                    }}
-                >
-                    <h1 className="mt-2 w-full text-center absolute top-0">
+                <motion.div className="col-span-1  border-[#373739]  h-full place-items-center w-full bg-black ">
+                    <motion.h1
+                        className="mt-2"
+                        initial={{
+                            borderTopWidth: 0,
+                            borderBottomWidth: 0,
+                            y: -100,
+                        }}
+                        animate={{
+                            y: 0,
+                        }}
+                        transition={{
+                            duration: 0.8,
+                            ease: "easeOut",
+                        }}
+                    >
                         Minha Agenda
-                    </h1>
-                    <div>
-                        <motion.button
-                            className="p-2 bg-amber-500 cursor-pointer text-white rounded"
-                            onClick={handleClick}
-                            animate={controls}
-                            whileTap={{ scale: 0.9 }}
-                            transition={{ type: "spring", stiffness: 300 }}
-                        >
-                            Adicionar
-                        </motion.button>
-                    </div>
+                    </motion.h1>
+                    <AnimatePresence mode="popLayout" initial={false}>
+                        {isVisible ? (
+                            <motion.div
+                                initial={{
+                                    opacity: 0,
+                                    scale: 0,
+                                }}
+                                animate={{ opacity: 1, scale: 1, y: 100 }}
+                                exit={{ opacity: 0, scale: 0 }}
+                                className="w-[300px] h-[100px] bg-[#373739] rounded-2xl"
+                                key="box"
+                            >
+                                <textarea
+                                    className="w-full h-full bg-transparent rounded-lg p-2 text-white outline-none resize-none"
+                                    placeholder="Digite aqui..."
+                                />
+                            </motion.div>
+                        ) : null}
+                        <div className="flex justify-center items-center w-full h-full">
+                            <motion.button
+                                className="p-2  border-b-2 border-amber-50 cursor-pointer flex items-center  text-white "
+                                onClick={() => setIsVisible(!isVisible)}
+                            >
+                                {isVisible ? "Fechar" : "Adicionar"}
+                            </motion.button>
+                        </div>
+                    </AnimatePresence>
                 </motion.div>
-                <motion.div
-                    className="col-span-1 border-l-4 border-r-4 border-[#373739] h-full place-items-center w-full bg-black relative"
-                    initial={{
-                        borderTopWidth: 0,
-                        borderBottomWidth: 0,
-                        y: -100,
-                    }}
-                    animate={{
-                        y: 0,
-                    }}
-                    transition={{
-                        duration: 0.8,
-                        ease: "easeOut",
-                    }}
-                >
-                    <h1 className="mt-2">Vendas</h1>
+                <motion.div className="col-span-1 border-l-4 border-r-4 border-[#373739] h-full place-items-center w-full bg-black relative">
+                    <motion.h1
+                        className="mt-2"
+                        initial={{
+                            borderTopWidth: 0,
+                            borderBottomWidth: 0,
+                            y: -100,
+                        }}
+                        animate={{
+                            y: 0,
+                        }}
+                        transition={{
+                            duration: 0.8,
+                            ease: "easeOut",
+                        }}
+                    >
+                        Vendas
+                    </motion.h1>
                 </motion.div>
-                <motion.div
-                    className="col-span-1  border-[#373739] h-full place-items-center w-full bg-black relative"
-                    initial={{
-                        y: -100,
-                    }}
-                    animate={{
-                        y: 0,
-                    }}
-                    transition={{
-                        duration: 0.8,
-                        ease: "easeOut",
-                    }}
-                >
-                    <h1 className="mt-2">Cadastros</h1>
+                <motion.div className="col-span-1  border-[#373739] h-full place-items-center w-full bg-black relative">
+                    <motion.h1
+                        className="mt-2"
+                        initial={{
+                            borderTopWidth: 0,
+                            borderBottomWidth: 0,
+                            y: -100,
+                        }}
+                        animate={{
+                            y: 0,
+                        }}
+                        transition={{
+                            duration: 0.8,
+                            ease: "easeOut",
+                        }}
+                    >
+                        Cadastros Clientes
+                    </motion.h1>
                 </motion.div>
             </div>
         </div>
